b58878d25708a4a38139c46d3d470ec125978c05
Jon Häggblad <jon.haggblad@gmail.com>
2020-10-21 14:44:05 +0200

    Backspace instead of ctrl on thumb key

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -106,7 +106,7 @@ pub static LAYERS: keyberon::layout::Layers = &[
         &[k(Tab),     k(Q), k(W),   k(E),   k(R), k(T),    k(Y),    k(U), k(I),    k(O),    k(P),     k(BSpace)],
         &[LCTL_ESC,   k(A), k(S),   k(D),   k(F), k(G),    k(H),    k(J), k(K),    k(L),    k(SColon),k(Quote) ],
         &[k(LShift),  k(Z), k(X),   k(C),   k(V), k(B),    k(N),    k(M), k(Comma),k(Dot),  k(Slash), k(RShift)],
-        &[Trans,      Trans,k(LGui),k(LAlt),l(1), k(Space),k(Enter),l(2), k(RAlt), k(RCtrl),Trans,    Trans    ],
+        &[Trans,      Trans,k(LGui),k(LAlt),l(1), k(Space),k(Enter),l(2), k(RAlt), k(BSpace),Trans,    Trans    ],
     ], &[
         &[s!(Grave), s!(Kb1),     s!(Kb2),     s!(Kb3),    s!(Kb4),    s!(Kb5),    s!(Kb6),   s!(Kb7),  s!(Kb8),  s!(Kb9), s!(Kb0),k(Delete)],
         &[Trans,     s!(LBracket),s!(RBracket),s!(Kb9),    s!(Kb0),    s!(Bslash), s!(Equal), k(Minus), s!(Minus),k(Equal),Trans,  Trans    ],
