cccc0f4083a091697f7533ff1f740c0707ba8c08
Jon Häggblad <jon.haggblad@gmail.com>
2020-10-29 14:56:52 +0100

    Add holdtap for shift and ctrl

--- firmware/src/layout.rs
+++ firmware/src/layout.rs
@@ -19,16 +19,40 @@ const RCTL_QUOT: Action = HoldTap {
     tap: &k(Quote),
 };
 
+const LCTL_A: Action = HoldTap {
+    timeout: 200,
+    hold: &k(LCtrl),
+    tap: &k(A),
+};
+
+const LSFT_Z: Action = HoldTap {
+    timeout: 200,
+    hold: &k(LShift),
+    tap: &k(Z),
+};
+
+const RCTL_SCOL: Action = HoldTap {
+    timeout: 250,
+    hold: &k(RCtrl),
+    tap: &k(SColon),
+};
+
+const RSFT_SLSH: Action = HoldTap {
+    timeout: 200,
+    hold: &k(RShift),
+    tap: &k(Slash),
+};
+
 const NEXT_TAB: Action = m(&[LCtrl, PgDown]);
 const PREV_TAB: Action = m(&[LCtrl, PgUp]);
 
 #[rustfmt::skip]
 pub static LAYERS: keyberon::layout::Layers = &[
     &[
-        &[k(Tab),     k(Q), k(W),   k(E),   k(R), k(T),    k(Y),    k(U), k(I),    k(O),    k(P),     k(BSpace)],
-        &[LCTL_ESC,   k(A), k(S),   k(D),   k(F), k(G),    k(H),    k(J), k(K),    k(L),    k(SColon),RCTL_QUOT],
-        &[k(LShift),  k(Z), k(X),   k(C),   k(V), k(B),    k(N),    k(M), k(Comma),k(Dot),  k(Slash), k(RShift)],
-        &[Trans,      Trans,k(LGui),k(LAlt),l(1), k(Space),k(Enter),l(2), k(RAlt), k(BSpace),Trans,    Trans    ],
+        &[k(Tab),     k(Q),  k(W),   k(E),   k(R), k(T),    k(Y),    k(U), k(I),    k(O),    k(P),     k(BSpace)],
+        &[LCTL_ESC,   LCTL_A,k(S),   k(D),   k(F), k(G),    k(H),    k(J), k(K),    k(L),    RCTL_SCOL,RCTL_QUOT],
+        &[k(LShift),  LSFT_Z,k(X),   k(C),   k(V), k(B),    k(N),    k(M), k(Comma),k(Dot),  RSFT_SLSH, k(RShift)],
+        &[Trans,      Trans, k(LGui),k(LAlt),l(1), k(Space),k(Enter),l(2), k(RAlt), k(BSpace),Trans,    Trans    ],
     ], &[
         &[s!(Grave), s!(Kb1),     s!(Kb2),     s!(Kb3),    s!(Kb4),    s!(Kb5),    s!(Kb6),   s!(Kb7),  s!(Kb8),  s!(Kb9), s!(Kb0), k(Delete)],
         &[Trans,     s!(LBracket),s!(RBracket),s!(Kb9),    s!(Kb0),    s!(Bslash), s!(Equal), k(Minus), s!(Minus),k(Equal),PREV_TAB,PREV_TAB ],
